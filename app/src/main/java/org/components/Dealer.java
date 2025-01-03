c8ca927d3dc1dfe7fc4195e72aa3f33de4331110
Dhruv <dg222jz@student.lnu.se>
2025-01-04 00:26:00 +0100

    GREEN: Added drawCards and calculateScore for Dealer, test passes.

--- app/src/main/java/org/components/Dealer.java
+++ app/src/main/java/org/components/Dealer.java
@@ -3,6 +3,8 @@ package org.components;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.enums.Value;
+
 public class Dealer {
     private final List<Card> hand;
 
@@ -18,11 +20,26 @@ public class Dealer {
         hand.add(card);
     }
 
-    public void drawCards(){
-
+    public void drawCards(Deck deck) {
+        while (calculateScore() < 17) {
+            Card card = deck.drawCard();
+            if (card == null) break; // Stop if deck is empty
+            dealCard(card);
+        }
     }
-
-    public void calculateScore(){
-
+    
+    public int calculateScore() {
+        int score = 0;
+        boolean hasAce = false;
+    
+        for (Card card : hand) {
+            score += card.getScore();
+            if (card.getValue() == Value.ACE) hasAce = true;
+        }
+    
+        // Adjust for ACE if score exceeds 21
+        if (score > 21 && hasAce) score -= 10;
+    
+        return score;
     }
-}
+}    

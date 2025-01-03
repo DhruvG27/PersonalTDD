6c47a4505a57c888ac1c39e0312680f3c7615d48
Dhruv <dg222jz@student.lnu.se>
2025-01-03 23:09:28 +0100

    Red: Added Dealer Class still Tests fail as Dependency on other Classes

---
+++ app/src/main/java/org/components/Dealer.java
@@ -0,0 +1,16 @@
+package org.components;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealer {
+    private final List<Card> hand;
+
+    public Dealer() {
+        this.hand = new ArrayList<>();
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+}

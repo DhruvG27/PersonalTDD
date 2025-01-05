9139bfa906d1c3c05ea741a8293e9878ac58db68
DhruvG27 <dhruv.gulabani@gmail.com>
2025-01-05 23:44:26 +0100

    REFACTOR: Refactor the logic to make it reusable for rendering both the dealer's and player's hands

--- app/src/main/java/org/components/EnglishView.java
+++ app/src/main/java/org/components/EnglishView.java
@@ -7,14 +7,19 @@ import java.util.stream.Collectors;
 
 public class EnglishView {
 
-    public String renderPlayerHand(Player player) {
-        // Reveal all cards in the player's hand
+    public String renderHand(String name, Player player) {
+        // Reveal all cards in the hand
         player.getHand().forEach(Card::show);
-
+    
         // Render the hand
-        return "Player's hand: " +
+        return name + "'s hand: " +
                player.getHand().stream()
                      .map(Card::toString)
                      .collect(Collectors.joining(", "));
     }
+
+    public String renderPlayerHand(Player player) {
+        return renderHand("Player", player);
+    }
+    
 }
